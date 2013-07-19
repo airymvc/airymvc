f5a2f9c61dd3007c6500a73af2f1e78bc8b88a5f
aaronchang.tw <aaronchang.tw@gmail.com>
2013-07-19 08:08:00 +0000

    add more config testing

--- tests/test_config.php
+++ tests/test_config.php
@@ -4,11 +4,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-require_once '../core/PathService.php';
-require_once '../config/lib/Config.php';
+require_once dirname(__FILE__) . '/../zframework/core/PathService.php';
+require_once dirname(__FILE__) . '/../zframework/config/lib/Config.php';
 
 $v1 = Config::getInstance()->getScriptPlugin();
-$v2 = Config::getInstance()->getDBConfig();
 var_dump($v1);
+
+$v2 = Config::getInstance()->getDBConfig();
 var_dump($v2);
+
+$v3 = Config::getInstance()->getDisplayError();
+var_dump($v3);
+
+$v4 = Config::getInstance()->getAuthenticationConfig();
+var_dump($v4);
+
 ?>

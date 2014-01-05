94c5283f0c30a974321eb58542a6455d60b6d72f
aaronchang.tw@gmail.com <aaronchang.tw@gmail.com@664e361d-b696-899b-ad6e-09a71d936bb0>
2014-01-05 10:22:52 +0000

    Fix Mysql access and add unit tests

--- trunk/tests/app/library/db/MysqlAccessTest.php
+++ trunk/tests/app/library/db/MysqlAccessTest.php
@@ -41,22 +41,44 @@ class MysqlAccessTest extends AiryUnitTest {
      */
     public function testWhere1() {
     	$condition = array("AND"=>array("="=>array("field1"=>"value1", "field2"=>"value2"), ">"=>array("field3"=>"value3")));
-		$result = $this->object->where($condition);
+		$result = $this->object->where($condition)->getWherePart();
 		$compare = " WHERE  `field1` = 'value1' AND `field2` = 'value2' AND `field3` > 'value3' ";
 		$this->assertEquals($compare, $result);
     }
     
     public function testWhere2() {		
 		$condition = array(""=>array("="=>array("field1" => "value1")));
-		$result = $this->object->where($condition);
+		$result = $this->object->where($condition)->getWherePart();
 		$compare = " WHERE  `field1` = 'value1' ";
 		$this->assertEquals($compare, $result);
     }
+    
+    public function testWhereStr1() {		
+		$condition = "`field1` > 2";
+		$result = $this->object->where($condition)->getWherePart();
+		$compare = " WHERE (`field1` > 2)";
+		$this->assertEquals($compare, $result);
+    }
+    
+    public function testWhereAnd() {		
+		$condition = "`field1` > 2";
+		$this->object->where($condition);
+		$this->object->andWhere("`field2` > 3");
+		$result = $this->object->orWhere("`field3` > 4")->getWherePart();
+		$compare = " WHERE (`field1` > 2) AND (`field2` > 3) OR (`field3` > 4)";
+		$this->assertEquals($compare, $result);
+    }
 
+    public function testSqlInjection() {
+		$condition = "' OR ''='";
+		$result = $this->object->andWhere($condition)->getWherePart();
+		$compare = " AND (\' OR \'\'=\')";
+		$this->assertEquals($compare, $result);    	
+    }
 
     public function testInnerJoin() {		
 		$tables = array("table1", "table2");
-		$result = $this->object->innerJoin($tables);		
+		$result = $this->object->innerJoin($tables)->getJoinPart();		
 		$compare = " INNER JOIN `table1` INNER JOIN `table2`";
 		$this->assertEquals($compare, $result);
     }
@@ -64,7 +86,7 @@ class MysqlAccessTest extends AiryUnitTest {
     public function testSelect1() {		
 		$table = "table1";
 		$columns = array ("count(*)", "col1", "col2");
-		$result = $this->object->select($columns, $table);		
+		$result = $this->object->select($columns, $table)->getSelectPart();		
 		$compare = "SELECT count(*), col1, col2 FROM `table1`";
 		$this->assertEquals($compare, $result);
     }  
@@ -72,11 +94,19 @@ class MysqlAccessTest extends AiryUnitTest {
     public function testSelect2() {		
 		$table = "table1";
 		$columns = array ("count(*)", "col1", "col2");
-		$result = $this->object->select($columns, $table, "distinct");		
+		$result = $this->object->select($columns, $table, "distinct")->getSelectPart();		
 		$compare = "SELECT DISTINCT count(*), col1, col2 FROM `table1`";
 		$this->assertEquals($compare, $result);
     }
-
+    
+    public function testSelectString1() {		
+		$table = "table1";
+		$columns = array ("count(*), col1, col2");
+		$result = $this->object->select($columns, $table, "distinct")->getSelectPart();		
+		$compare = "SELECT DISTINCT count(*), col1, col2 FROM `table1`";
+		$this->assertEquals($compare, $result);
+    }
+    
     public function testJoinOn1() {	
 
     	$condition = array ("AND" => array(array( "=", 'table1'=>'field1', 'table2'=>'field2'), 
@@ -84,7 +114,7 @@ class MysqlAccessTest extends AiryUnitTest {
                       					   array("<>", 'table4'=>'field4', 'table3'=>'field3')), 
                 			 "OR" => array(array( "=", 'table5'=>'field5', 'table6'=>'field6')));
 
-		$result = $this->object->joinOn($condition);		
+		$result = $this->object->joinOn($condition)->getJoinOnPart();		
 		$compare = " ON  `table1`.`field1` = `table2`.`field2` AND `table3`.`field3` <> `table2`.`field2` AND `table4`.`field4` <> `table3`.`field3`  OR `table5`.`field5` = `table6`.`field6` ";
 		$this->assertEquals($compare, $result);
     }    
@@ -97,10 +127,64 @@ class MysqlAccessTest extends AiryUnitTest {
     	                                )
     	                    );
 
-		$result = $this->object->joinOn($condition);		
+		$result = $this->object->joinOn($condition)->getJoinOnPart();		
 		$compare = " ON  `table1`.`field1` = `table2`.`field2` ";
 		$this->assertEquals($compare, $result);
     } 
+    
+    public function testJoinOnString1() {	
+
+    	$condition = "`table1`.`field1` = `table2`.`field2`";
+
+		$result = $this->object->joinOn($condition)->getJoinOnPart();		
+		$compare = " ON `table1`.`field1` = `table2`.`field2`";
+		$this->assertEquals($compare, $result);
+    } 
+
+    public function testLimit() {	
+		$result = $this->object->limit(10, 5)->getLimitPart();		
+		$compare = " LIMIT 10, 5";
+		$this->assertEquals($compare, $result);
+    } 
+    
+    public function testGroupBy() {	
+		$result = $this->object->groupBy("abc")->getGroupPart();		
+		$compare = " GROUP BY abc";
+		$this->assertEquals($compare, $result);
+    } 
+    
+    public function testOrderBy() {	
+		$result = $this->object->orderBy("abc")->getOrderPart();		
+		$compare = " ORDER BY abc";
+		$this->assertEquals($compare, $result);
+		
+		$result = $this->object->orderBy("abc", 1)->getOrderPart();		
+		$compare = " ORDER BY abc DESC";
+		$this->assertEquals($compare, $result);
+    } 
+    
+    public function testInWhere() {	
+		$this->object->InWhere("abc, def");
+		$result = $this->object->getWherePart();		
+		$compare = " IN (abc, def)";
+		$this->assertEquals($compare, $result);
+    } 
+    
+    public function testUpdate() {
+    	$columns = array('column_name' => 'column_value', 'column_name1' => 'column_value1');
+    	$table = 'table';	
+		$result = $this->object->update($columns, $table)->getUpdatePart();		
+		$compare = "UPDATE `table` SET `column_name`='column_value', `column_name1`='column_value1'";
+		$this->assertEquals($compare, $result);
+    }
+
+    public function testInsert() {
+    	$columns = array('column_name' => 'column_value', 'column_name1' => 'column_value1');
+    	$table   = 'table';	
+		$result  = $this->object->insert($columns, $table)->getInsertPart();		
+		$compare = "INSERT INTO table ( `column_name`, `column_name1`) VALUES ('column_value', 'column_value1')";
+		$this->assertEquals($compare, $result);
+    }    
     
     public function testGetStatement1() {
     	$columns1 = array(0 => 'attendent.id',
@@ -133,6 +217,47 @@ class MysqlAccessTest extends AiryUnitTest {
 		$this->object->where($where1);
 		$result = $this->object->getStatement();
 		$compare = "SELECT attendent.id, attendent.img, attendent.name, award.name, award.annotation, activity_mng.attend_date FROM `attendent` INNER JOIN `activity_mng` INNER JOIN `award` ON  `award`.`id` = `activity_mng`.`award_id` AND `attendent`.`id` = `activity_mng`.`attendent_id`  WHERE  `activity_id` = '5' AND `award_id` > '0' ";
+		$this->assertEquals($compare, $result);
+    }
+    
+    public function testGetStatement2() {
+    	$columns1 = ("'attendent.id','attendent.img','attendent.name','award.name','award.annotation','activity_mng.attend_date'");
+    					  
+		$joinTables1 = array(0 => 'activity_mng',
+                     		 1 => 'award',);
+
+		$this->object->select($columns1, 'attendent')
+					 ->innerJoin($joinTables1)
+					 ->joinOn("`award`.`id` = `activity_mng`.`award_id`")
+					 ->andJoinOn("`attendent`.`id` = `activity_mng`.`attendent_id`")
+					 ->where("`activity_id` = 5")
+					 ->andWhere("`award_id` > 0");
+
+		$result = $this->object->getStatement();
+		$compare = "SELECT 'attendent.id','attendent.img','attendent.name','award.name','award.annotation','activity_mng.attend_date' FROM `attendent` INNER JOIN `activity_mng` INNER JOIN `award` ON `award`.`id` = `activity_mng`.`award_id` AND `attendent`.`id` = `activity_mng`.`attendent_id` WHERE (`activity_id` = 5) AND (`award_id` > 0)";
+		$this->assertEquals($compare, $result);
+    }
+    
+    public function testGetStatement3() {
+    	$columns1 = array(0 => 'attendent.id',
+    					  1 => 'attendent.img',
+    					  2 => 'attendent.name',
+    					  3 => 'award.name',
+    					  4 => 'award.annotation',
+    					  5 => 'activity_mng.attend_date');
+    					  
+		$joinTables1 = array(0 => 'activity_mng',
+                     		 1 => 'award',);
+
+		$this->object->select($columns1, 'attendent')
+					 ->innerJoin($joinTables1)
+					 ->joinOn("`award`.`id` = `activity_mng`.`award_id`")
+					 ->andJoinOn("`attendent`.`id` = `activity_mng`.`attendent_id`")
+					 ->where("`activity_id` = 5")
+					 ->andWhere("`award_id` > 0");
+
+		$result = $this->object->getStatement();
+		$compare = "SELECT attendent.id, attendent.img, attendent.name, award.name, award.annotation, activity_mng.attend_date FROM `attendent` INNER JOIN `activity_mng` INNER JOIN `award` ON `award`.`id` = `activity_mng`.`award_id` AND `attendent`.`id` = `activity_mng`.`attendent_id` WHERE (`activity_id` = 5) AND (`award_id` > 0)";
 		$this->assertEquals($compare, $result);
     }
 }
